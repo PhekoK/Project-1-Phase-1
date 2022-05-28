44d4becf2f7fa8c29b1a6d3228d5e10a16a4ce43
Karabo Pheko <216021429@student.uj.ac.za>
2022-05-29 01:10:26 +0200

    Added the project main java file

---
+++ src/com/project1/main/ProjectMain.java
@@ -0,0 +1,16 @@
+package com.project1.main;
+
+import java.util.Scanner;
+
+public class ProjectMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a value: ");
+		String name = scan.next();
+		System.out.println("Welcome, "+name + " :)");
+
+	}
+
+}
